00c6e4df52d6331268b5d879ec15b146c7879f4d
Matias <matiascardozo14300@gmail.com>
2020-10-16 08:23:54 -0300

    Card de Calendario Hecha

--- src/Components/Calendario/Calendario.jsx
+++ src/Components/Calendario/Calendario.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+//import "bootstrap/dist/css/bootstrap.min.css";
+import "./Calendario.scss";
 
 const Calendario = () => {
    return (
-      <div>
-         Calendario
+      <div className="calendar">
+         
+         <div className="mi-calendario">
+            <p>Mi Calendario</p>
+            <img className="imagen" src="calendar.png" width="433px" height="384px"></img>
+         </div>
+
+         <div className="turno">
+
+         </div>
+
       </div>
+
    );
 };
 
