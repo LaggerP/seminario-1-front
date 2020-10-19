bb4fee4e5da1bfd150adbcf70eeead5fa5fd22bd
NicolasCamicha22 <Nicolascamicha22@gmail.com>
2020-10-19 17:21:45 -0300

    New excercises done

--- src/Components/Administrar/Modal/ProfileExerciseAssignmentModal.jsx
+++ src/Components/Administrar/Modal/ProfileExerciseAssignmentModal.jsx
@@ -1,10 +1,14 @@
 import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button'
 import Modal from 'react-bootstrap/Modal'
-import Form from 'react-bootstrap/Form'
+import './ProfileExerciseAssignmentModal.scss'
+import Card from 'react-bootstrap/Card';
+
+
 
 
 const ProfileExerciseAssignmentModal = (props) => {
+   const [show, setShow] = useState(false);
    const [profileData, setProfileData] = useState({
       profile_name: "",
       firstname: "",
@@ -13,61 +17,94 @@ const ProfileExerciseAssignmentModal = (props) => {
       birthday:""
    })
 
-   const handleChange = (e) => {
-      const { name, value } = e.target;
-      setProfileData({
-         ...profileData,
-         [name]: value
-      });
-   }
+const handleClose = () => setShow(true);
+const handleShow = () => setShow(true);
 
-   const addProfile = () => {
-      props.setResponsableData({
-         ...props.responsableData,
-         profiles: profileData
-      })
-      props.onHide();
-   }
+
+
+
+
+  
+
+
+
 
    return (
-      <Modal
-         {...props}
-         size="md"
-         aria-labelledby="contained-modal-title-vcenter"
-         centered
+      <Modal 
+      {...props}
+      size='md'
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
       >
-         <Modal.Header closeButton>
-            <Modal.Title id="contained-modal-title-vcenter">
-               Asignar ejercicio
-            </Modal.Title>
-         </Modal.Header>
-         <Modal.Body>
-            <Form>
-               <Form.Group controlId="">
-                  <Form.Label>Nombre del usuario del paciente</Form.Label>
-                  <Form.Control type="text" placeholder="Ingrese nombre del usuario del paciente" name="profile_name" value={profileData.profile_name} onChange={handleChange} />
-               </Form.Group>
-               <Form.Group controlId="">
-                  <Form.Label>DNI</Form.Label>
-                  <Form.Control type="text" placeholder="Ingrese DNI" name="dni" value={profileData.dni} onChange={handleChange} />
-               </Form.Group>
-               <Form.Group controlId="">
-                  <Form.Label>Fecha nacimiento</Form.Label>
-                  <Form.Control type="date" placeholder="Fecha de nacimiento" name="birthday" value={profileData.birthday} onChange={handleChange} />
-               </Form.Group>
-               <Form.Group controlId="">
-                  <Form.Label>Nombre(s)</Form.Label>
-                  <Form.Control type="text" placeholder="Ingrese nombre(s)" name="firstname" value={profileData.firstname} onChange={handleChange} />
-               </Form.Group>
-               <Form.Group controlId="">
-                  <Form.Label>Apellido(s)</Form.Label>
-                  <Form.Control type="text" placeholder="Apellido(s)" name="lastname" value={profileData.lastname} onChange={handleChange} />
-               </Form.Group>
-            </Form>
+    
+      <Modal.Header closeButton>
+        <Modal.Title id= "contained-modal-title-vcenter">
+           Asignar ejercicios
+         </Modal.Title>
+     
+      </Modal.Header>
+      <Modal.Body>
+         
+        <div className='cardsContainer'> 
+      <Card className ='Ejercicios'>
+      <div class="custom-control custom-checkbox">
+          <input type="checkbox"  id="defaultUnchecked"/>
+          <label  for="defaultUnchecked">Ejercicio Numero 1</label>
+          </div>
+            
+      </Card>
 
-            <Button variant="info" size="sm" block onClick={() => addProfile()}>+ Agregar perfil</Button>{' '}
-         </Modal.Body>
-      </Modal>
+   
+      <Card className ='Ejercicios'>
+      <div class="custom-control custom-checkbox">
+          <input type="checkbox"  id="customControlAutosizing"/>
+          <label  for="customControlAutosizing">Ejercicio Numero 2</label>
+          </div>
+      </Card>
+
+      <Card className ='Ejercicios'>
+      <div class="custom-control custom-checkbox">
+          <input type="checkbox" id="defaultUnchecked3"/>
+          <label  for="defaultUnchecked3">Ejercicio Numero 3</label>
+          </div>
+      </Card>
+      
+      <Card className ='Ejercicios'>
+      <div class="custom-control custom-checkbox">
+          <input type="checkbox" id="defaultUnchecked4"/>
+          <label  for="defaultUnchecked4">Ejercicio Numero 4</label>
+          </div>
+
+      </Card>
+      
+      <Card className ='Ejercicios'>
+      <div class="custom-control custom-checkbox">
+          <input type="checkbox"  id="defaultUnchecked5"/>
+          <label  for="defaultUnchecked5">Ejercicio Numero 5</label>
+          </div>
+      </Card>
+        
+      <Card className ='Ejercicios'>
+      <div class="custom-control custom-checkbox">
+          <input type="checkbox"  id="defaultUnchecked6"/>
+          <label  for="defaultUnchecked6">Ejercicio Numero 6</label>
+          </div>
+      </Card>
+
+      </div>
+      </Modal.Body>
+
+          <Modal.Footer>      
+            <Button variant="info" size="sm" onClick={handleClose}>
+                Cerrar
+              </Button>
+              <Button variant="info" size="sm" onClick={handleClose}>
+                Guardar cambios
+              </Button>
+            </Modal.Footer>
+          </Modal>
+         
+
    );
 }
 
