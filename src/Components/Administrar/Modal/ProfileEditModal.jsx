089b29d8e324222e7fb52960dbcb2e5f24abb76b
TamaraCabello <56520799+TamaraCabello@users.noreply.github.com>
2020-10-19 15:20:57 -0300

    Added modals exercise, visit and edit profile

---
+++ src/Components/Administrar/Modal/ProfileEditModal.jsx
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button'
+import Modal from 'react-bootstrap/Modal'
+import Form from 'react-bootstrap/Form'
+
+
+const ProfileEditModal = (props) => {
+   const [profileData, setProfileData] = useState({
+      profile_name: "",
+      firstname: "",
+      lastname: "",
+      dni:"",
+      birthday:""
+   })
+
+   const handleChange = (e) => {
+      const { name, value } = e.target;
+      setProfileData({
+         ...profileData,
+         [name]: value
+      });
+   }
+
+   const addProfile = () => {
+      props.setResponsableData({
+         ...props.responsableData,
+         profiles: profileData
+      })
+      props.onHide();
+   }
+
+   return (
+      <Modal
+         {...props}
+         size="md"
+         aria-labelledby="contained-modal-title-vcenter"
+         centered
+      >
+         <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-vcenter">
+               Editar paciente
+            </Modal.Title>
+         </Modal.Header>
+         <Modal.Body>
+            <Form>
+               <Form.Group controlId="">
+                  <Form.Label>Nombre del usuario del paciente</Form.Label>
+                  <Form.Control type="text" placeholder="Ingrese nombre del usuario del paciente" name="profile_name" value={profileData.profile_name} onChange={handleChange} />
+               </Form.Group>
+               <Form.Group controlId="">
+                  <Form.Label>DNI</Form.Label>
+                  <Form.Control type="text" placeholder="Ingrese DNI" name="dni" value={profileData.dni} onChange={handleChange} />
+               </Form.Group>
+               <Form.Group controlId="">
+                  <Form.Label>Fecha nacimiento</Form.Label>
+                  <Form.Control type="date" placeholder="Fecha de nacimiento" name="birthday" value={profileData.birthday} onChange={handleChange} />
+               </Form.Group>
+               <Form.Group controlId="">
+                  <Form.Label>Nombre(s)</Form.Label>
+                  <Form.Control type="text" placeholder="Ingrese nombre(s)" name="firstname" value={profileData.firstname} onChange={handleChange} />
+               </Form.Group>
+               <Form.Group controlId="">
+                  <Form.Label>Apellido(s)</Form.Label>
+                  <Form.Control type="text" placeholder="Apellido(s)" name="lastname" value={profileData.lastname} onChange={handleChange} />
+               </Form.Group>
+            </Form>
+
+            <Button variant="info" size="sm" block onClick={() => addProfile()}>+ Agregar perfil</Button>{' '}
+         </Modal.Body>
+      </Modal>
+   );
+}
+
+
+export default ProfileEditModal
--- src/Components/Administrar/PacientesMock.jsx
+++ src/Components/Administrar/PacientesMock.jsx
@@ -1,8 +1,8 @@
 export const ResponsablesMock = [
     {
         id: 1,
-        nombre: 'Sergio',
-        apellido:'Dopazo',
+        nombre: 'Martin',
+        apellido:'McVuelo',
         documento: '52145896'
     },
     {
@@ -24,16 +24,16 @@ export const PacientesMock = [
     {
         id: 1,
         idResponsable: 1,
-        nombre: "Anastasia",
-        apellido: "Dopazo",
+        nombre: "Gonzalo",
+        apellido: "McVuelo",
         fechaNacimiento: "27/07/2010",
         documento: '84125369'
     },
     {
         id: 2,
         idResponsable: 1,
-        nombre: "Aurora",
-        apellido: "Dopazo",
+        nombre: "Isabel",
+        apellido: "McVuelo",
         fechaNacimiento: "02/01/2017",
         documento: '84125963'
     },
@@ -42,7 +42,7 @@ export const PacientesMock = [
         idResponsable: 2,
         nombre: "Tomas",
         apellido: "Malio",
-        fechaNacimiento: "27/07/1994",
+        fechaNacimiento: "27/07/1992",
         documento: '25413689'
     },
     {
@@ -50,7 +50,7 @@ export const PacientesMock = [
         idResponsable: 2,
         nombre: "Rodrigo",
         apellido: "Dos Santos",
-        fechaNacimiento: "02/01/1996",
+        fechaNacimiento: "02/01/1994",
         documento: '36521456'
     },
     {
@@ -76,5 +76,21 @@ export const PacientesMock = [
         apellido: "Carrera",
         fechaNacimiento: "26/05/1999",
         documento: '14523698'
+    },
+    {
+        id: 6,
+        idResponsable: 3,
+        nombre: "Tamara",
+        apellido: "Cabello",
+        fechaNacimiento: "17/06/1999",
+        documento: '41256352'
+    },
+    {
+        id: 7,
+        idResponsable: 3,
+        nombre: "Pablo",
+        apellido: "Lagger",
+        fechaNacimiento: "17/07/1997",
+        documento: '36259845'
     },
 ]
