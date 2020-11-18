3bbd72745ab839fc7677dc289e02e894f0f00f66
Nicolas Camicha22 <Nicolascamicha22@gmail.com>
2020-11-18 18:07:01 -0300

    Seguimiento que tare ejercicios reales pero no la fecha

--- src/Components/Seguimiento/ModalFecha.jsx
+++ src/Components/Seguimiento/ModalFecha.jsx
@@ -4,16 +4,67 @@ import Modal from 'react-bootstrap/Modal'
 import Form from 'react-bootstrap/Form'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-import Alert from 'react-bootstrap/Alert'
 import Collapse from 'react-bootstrap/Collapse'
+import Badge from 'react-bootstrap/Badge'
+
+import SeguimientoCard from './SeguimientoCard';
+import { getExerciseDone } from '../../Api/services/administrarServices';
+
+import { getExercisesByProfile } from '../../Api/services/exerciseServices'
+import {getAllExerciseHistory} from '../../Api/services/historyServices'
 
 
 const ProfileVisitAssignmentModal = (props) => {
-   const [open, setOpen] = useState(false);
+   console.log('props:', props)
+   const [consigna, setConsigna] = useState(false);
+   const [showExercise, setShowExercise] = useState(true);
+   const [selectedOption, setSelectedOption] = useState(null);
+   const [exercises, setExercises] = useState([]);
+   
+   const [historia, setHistoria] = React.useState({});
+
+   const getExercise = async () => {
+      const data = await getExercisesByProfile(props.profileId,props.profileFecha)
+      console.log("getExercise", data)
+      setDataExercise(data.profileExercises)
+   }
+
+   const getHistoryExercise = async () => {
+      const data = await getAllExerciseHistory()
+      setHistoria(data)
+   }
+
+   const [allExercises, setAllExercises] = useState([]);
+
+   const [dataExercise, setDataExercise] = useState([]);
+
    const [profileData, setProfileData] = useState({
       visit_date: "",
-   
+      exercises: [],
+
+
    })
+
+
+
+   const addVisit = () => {
+      setConsigna(!consigna)
+      const dateValue = document.querySelector('input[type="date"]').value;
+      console.log(profileData.visit_date)
+      console.log("Fecha seleccionada:", dateValue)
+      props.onHide()
+
+   }
+
+
+   const getGameData = async (e) => {
+      await setDataExercise(e);
+      setShowExercise(false);
+      const exerciseDone = document.querySelector('input[type="exercise"]').value;
+      console.log(profileData.exerciseDone)
+
+   }
+
 
    const handleChange = (e) => {
       const { name, value } = e.target;
@@ -21,23 +72,28 @@ const ProfileVisitAssignmentModal = (props) => {
          ...profileData,
          [name]: value
       });
+
    }
 
-   // const handleSubmit = () => {
-   //    // addVisit();
-   //    var dateValue = document.querySelector('input[type="date"]').value;
-   //    var timeValue = document.querySelector('input[type="time"]').value;
-   //    console.log(dateValue, timeValue);
-   //    props.onHide()
-   // }
+
+   React.useEffect(function effectFunction() {
+      console.log("Cambiando Formulario", profileData);
+      console.log()
+      if (profileData.visit_date) {
+         getExercise()
+
+         //const data = await getExercisesByProfile(props.profileId)
 
 
-   
-   const addVisit = () => {
-      var dateValue = document.querySelector('input[type="date"]').value;
-      props.onHide()
-   }
-  
+         //setExercises(data.profileExercises);
+         //setAllExercises(data.profileExercises);
+      }
+   }, [profileData]);
+
+
+
+
+
 
    return (
       <Modal
@@ -52,36 +108,67 @@ const ProfileVisitAssignmentModal = (props) => {
             </Modal.Title>
          </Modal.Header>
          <Modal.Body>
-            {/*onSubmit={handleSubmit} */}
             <Form>
-               <Form.Group controlId="" style={{marginTop:0}}>
-                  <Form.Label>Fecha en la que se realizo en el ejercicio</Form.Label>
-                  <Form.Control type="date" name="visit_date" value={profileData.visit_date} onChange={handleChange} required />
+               <Form.Group controlId="" style={{ marginTop: 0 }}>
+
+                  <h1>Seleccionar fecha</h1>
+
                </Form.Group>
-              
+
                <Row>
                   <Col xs={12} md={12}>
-                     <Button variant="success" onClick={() => setOpen(!open)} aria-controls="example-collapse-text" aria-expanded={open} size="sm" block >Buscar</Button>{' '}
-                     <Collapse in={open}>
-                     <div id="example-collapse-text">
-                       El dia de la fecha el paciente realizo los ejercicios: 2 - 5 - 4 del modulo: 3
-                     </div>
+                     <Button variant="success" onClick={() => setConsigna(!consigna)} aria-controls="example-collapse-text" aria-expanded={consigna} size="sm" block >Abrir calendario</Button>{' '}
+                     <br/>
+                     <Collapse in={consigna}>
+                        <div id="example-collapse-text">
+                           <div className="CardContainer-Badges">
+                              <Form>
+                                 <Form.Control type="date" name="visit_date" value={profileData.visit_date} onChange={handleChange} required />
+                                 <br/>
+                                 {dataExercise.map(exercise => {
+                                    console.log(historia)
+                                    if(exercise.finished==true){
+                                       return <Badge variant="success" > En la fecha: {(profileData.visit_date)} <br />el parciente realizo los Ejercicios: <br /> {exercise.name} <br /></Badge>
+                                    } 
+                                    }
+                                 )}
+                              </Form>
+                           </div>
+
+                        </div>
                      </Collapse>
-                  
-                    
-                     
-                  
-
-
-                  
-         
-               </Col>
+                  </Col>
                </Row>
+
+               <Row style={{ paddingTop: '2.5%' }}>
+                  {
+                     exercises.map((data, index) => {
+                        const { id, name, description, consigna, image, module, finished } = data
+                        console.log(image)
+                        return (
+                           <Col xs={12} md={4} key={data.id} value={data} onClick={() => { getGameData(data) }}>
+                              <SeguimientoCard
+                                 name={name}
+                                 description={description}
+                                 consigna={consigna}
+                                 module={module}
+                                 finished={finished}
+                                 image={image}
+                                 id={id}
+                              />
+                           </Col>
+                        )
+                     })
+                  }
+               </Row>
+
             </Form>
          </Modal.Body>
       </Modal>
    );
+
 }
+
 
 
 export default ProfileVisitAssignmentModal
--- src/Components/Seguimiento/Seguimiento_VistaPaciente.jsx
+++ src/Components/Seguimiento/Seguimiento_VistaPaciente.jsx
@@ -1,118 +1,105 @@
-import React from 'react';
-import "./Seguimiento.scss" 
-import BootstrapTable from 'react-bootstrap-table-next';
-import ToolkitProvider, { Search } from 'react-bootstrap-table2-toolkit';
+import React, { useState, useEffect } from 'react';
+import "./Seguimiento.scss"
 import {
    Container,
    Row,
+   Table
 } from 'react-bootstrap';
-import Seguimientoimagee from '../../assets/images/Seguimientoimagee.png'
-
- 
-
-const { SearchBar } = Search;
-const columns = [{
-  dataField: 'id',
-  text: 'Fecha'
-}, {
-  dataField: 'price',
-  text: 'Modulo'
-},{
-   dataField: 'price2',
-   text: 'Ejercicios'
-}];
+import { getProfileData } from '../../Api/services/authService';
+import * as moment from 'moment';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
+import Tooltip from 'react-bootstrap/Tooltip'
+import ProfileVisitAssignmentModal from './ModalFecha';
+import { BsCalendar } from "react-icons/bs";
+import { getRol } from '../../Api/services/authService'
 
 
 
 
 
+const Seguimiento_VistaPaciente = ({ id, firstname, lastname, dni, birthday, profiles, profileData }) => {
+   const [modalVisitAssignment, setModalVisitAssignment] = React.useState(false);
+   const rol = getRol();
+   const [responsableData, setResponsableData] = useState({
+      profiles: [],
+   })
 
 
-const products = [{
-    id:  '21/10/2020', 
-    price: '1', 
-    price2: '2,1,7',
-  
-   },{
-    id: '21/10/2020', 
-    price: '2', 
-    price2: '2,8',
-
-     },{
-    id: '14/10/2020', 
-    price: '2',
-    price2: '3',
-
-   },{
-    id: '11/10/2020',
-    price: '3',
-    price2: '4', 
- 
- 
-
-   },{
-    id: '09/10/2020',
-    price: '1',
-    price2: '5,6',
-
-}];
+   const [profileId, setProfileId] = React.useState(false);
 
 
-
-
-
-const Seguimiento = () => {
+   console.log('Aca',profileData)
    return (
-      <div>    
-         <Container>
-            <Row> 
-          <div className="SeguimientoContainer-Bienvenida">
-          <div className="SeguimientoContainer-Bienvenida-Texto">
-        <h1> Hisotrial del paciente: </h1>
-         </div>
-         <h2>Bienvenido a la pantalla de seguimiento de ejercicios</h2>
+      <div>
+         <Table className='table-style'>
+            <thead>
+               <tr>
+                  <th>DNI</th>
+                  <th>Nombre</th>
+                  <th>Apellido</th>
+                  <th>Fecha de Nacimiento</th>
 
-         <div className="SeguimientoContainer-Bienvenida-Imagen">
-         <img src={Seguimientoimagee} height='340px' alt="Seguimientoimagee" />
-         </div> 
-         </div>
-         </Row>  
-         <div className='cardsContainer'> 
-            <ToolkitProvider
-            keyField="id"
-            columns={ columns }
-            data={ products }
-            search
-            
-            >
-            {
-               props => (
-                  <div>
-                  <h3>Buscar la fecha en la que se realizo tal ejercicio...:</h3>
-                  <SearchBar { ...props.searchProps } 
-                  className="custome-search-field"
-                  placeholder = 'Ingrese fecha para buscar el ejercicio'
-                  />
-                  <hr />
-                  <BootstrapTable
-                     { ...props.baseProps }
+
+
+               </tr>
+            </thead>
+
+
+                     <tbody>
+                        <tr>
+                           <td>{profileData.dni}</td>
+                           <td>{profileData.firstname}</td>
+                           <td>{profileData.lastname}</td>
+                           <td>{moment(profileData.birthday).format('DD/MM/YYYY')}</td>
+
+
+
+
+                           <td>
+                              <Row className='flex-row-reverse'>
+                                 <OverlayTrigger overlay={<Tooltip>Filtrar fecha</Tooltip>}>
+
+                                    <div className='icon-size' onClick={() => {
+                                       setProfileId(id)
+                                       setModalVisitAssignment(true)
+
+
+                                    }
+                                    }>
+                                       <BsCalendar className='icon-styles' /></div>
+                                 </OverlayTrigger>
+                              </Row>
+                           </td>
+
+                        </tr>
+                     </tbody>
+
+
                 
-                  />
-                  </div>
-               )
-            }
-         </ToolkitProvider>
-         </div>
-         </Container>
-   
-            
-      
-         
-         
- </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+         </Table>
+         <ProfileVisitAssignmentModal
+            show={modalVisitAssignment}
+            onHide={() => setModalVisitAssignment(false)}
+            profileId={profileData.id}
+         />
+
+
+      </div>
    );
+
 };
 
-
-
-export default Seguimiento;
+export default Seguimiento_VistaPaciente;
